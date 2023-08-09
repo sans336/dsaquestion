fc34cde4b5c422aec0d3a6a07da2d0ca0ed0a80a
sans336 <sanskritij76@gmail.com>
2023-08-09 14:52:42 +0530

    day-17 of 24 days of code

---
+++ 24-days/day-17/1.js
@@ -0,0 +1,105 @@
+// 1 . Star Pattern
+// *****
+// ****
+// ***
+// **
+// *
+
+// 2. Star Pattern
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// 3. Number Pattern
+// 11111
+// 2222
+// 333
+// 44
+// 5
+
+// 4. Number pattern
+// 12345
+// 2345
+// 345
+// 45
+// 5
+
+// 5. Star Pattern
+// *
+// **
+// ***
+// ****
+// *****
+
+//6
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+           //answers 
+
+// 5 //
+// let n = 5;
+// let str = "";
+// for (i =0; i <= n; i++) {
+//   str = str + "*";
+//   console.log(str);
+// }
+
+// 1 //
+// let n = 5;
+// let str = "";
+// for (i = 0; i < n; i++) {
+//     for (j = 0; j <n-i; j++){
+//         str = str + "*"
+
+//     } str= str+"\n";
+
+// } console.log(str)
+
+//2//
+// let n = 5;
+// let num = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     num = num + "*";
+//   }
+//   num = num + "\n";
+// }
+// console.log(num);
+
+
+//6
+// let n = 5;
+// let str = "";
+// for (i = 1; i <= 5; i++) {
+//   str += i;
+//   console.log(str);
+// }
+
+//3
+
+// let n = 6;
+// let str = "";
+// for (i = 1; i <= n; i++) {
+//   for (j = 1; j <= n - i; j++) {
+//     str = str + i;
+//   } str= str+"\n"
+
+// } console.log(str);
+
+//4
+
+// let n = 6;
+// let str = "";
+// for (i = 1; i < n; i++) {
+//   for (j = i; j<n; j++) {
+//     str = str+j;
+//   }
+//   str = str + "\n";
+// }
+// console.log(str);
